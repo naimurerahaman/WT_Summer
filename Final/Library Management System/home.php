8f4254ad725245ebd81472dd8329e9306a1c5f0a
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-08-21 20:05:07 +0600

    Project file created and home page added

---
+++ Final/Library Management System/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Library Management System</title>
+  <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+  <!-- Header -->
+  <header class="header">
+    <div class="logo">MyLibrary</div>
+    
+    <div class="auth-buttons">
+      <button class="btn login">Login</button>
+      <button class="btn signup">Signup</button>
+    </div>
+  </header>
+
+  <section class="about">
+    <div class="about-us">
+      <h1>About Us</h1>
+      <p class="aboutUs-Para">MyLibrary is a non-profit online platform dedicated to making knowledge accessible to everyone, everywhere. <br>
+        We offer a vast digital library of educational, motivational, and career-focused books—completely free of charge.  <br>
+        Our mission is to empower individuals through reading and lifelong learning, helping them grow personally and professionally. <br>
+        Whether you're a student, job seeker, entrepreneur, or lifelong learner, MyLibrary provides the resources to keep you inspired  <br>
+        and informed in every area of life. <br>
+        Over the years, we have continued to grow—becoming not only a trusted hub for readers but also a space where  <br>
+        self-publishing authors can share their work with a global audience.  <br>
+        If you have any questions, suggestions, or would like to partner with us, please contact our editorial team at info@mylibrary.com</p>  
+    </div>
+  </section>
+
+  <footer class="footer">
+    <h3>Stay Connected</h3>
+    <div class="social-links">
+
+    </div>
+    <p>&copy; 2025 Library Management System. All rights reserved.</p>
+  </footer>
+
+</body>
+</html>
